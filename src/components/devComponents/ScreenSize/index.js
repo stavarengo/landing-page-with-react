474cf00ef48b2601f1eab7578d7b3460e4985095
stavarengo <faelsta@gmail.com>
2019-05-31 11:48:54 -0300

    Create src/components/devComponents/ScreenSize

---
+++ src/components/devComponents/ScreenSize/index.js
@@ -0,0 +1,18 @@
+import IconButton from '@material-ui/core/IconButton';
+import React from 'react';
+import DeveloperModeIcon from '@material-ui/icons/DeveloperMode';
+import Badge from '@material-ui/core/Badge';
+import Tooltip from '@material-ui/core/Tooltip';
+import withWidth from '@material-ui/core/withWidth';
+
+const ScreenSize = withWidth()(({ theme, width }) => (
+  <Tooltip title={`Your screen width is ${width}`}>
+    <IconButton disableRipple>
+      <Badge badgeContent={width}>
+        <DeveloperModeIcon />
+      </Badge>
+    </IconButton>
+  </Tooltip>
+));
+
+export default ScreenSize;
